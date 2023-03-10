53a588e34508d9e95801ccc02d9e185c01a772c4
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-03-10 16:18:39 +0100

    full list of data

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,20 @@
 const myHeaders = new Headers();
 myHeaders.append("X-Api-Key", "lDFCyh4KEikWVdOsZYetvp50_QSX2Rnq");
 
-const formdata = new FormData();
-formdata.append("price", "20");
-formdata.append("bedrooms", "1");
-formdata.append("bathrooms", "1");
-formdata.append("size", "1");
-formdata.append("streetName", "Overtoom");
-formdata.append("houseNumber", "21");
-formdata.append("numberAddition", "1");
-formdata.append("zip", "1181TY");
-formdata.append("city", "Amsterdam");
-formdata.append("constructionYear", "1960");
-formdata.append("hasGarage", "false");
-formdata.append("description", "Nice house!");
-
 const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: formdata,
-    redirect: 'follow'
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
 };
 
 fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
-.then((response) => response.json())
-.then((result) => {
-    console.log(result);
-    changeData(result);
-});
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        changeData(result);
+    })
+  .catch(error => console.log('error', error));
 
 function changeData(result) {
-    
+
 }
